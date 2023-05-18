6c21f4255a0454d5b885cd76f9bb74001ba88e1a
Giulia Doria <giulia.doria.1@studenti.unipd.it>
2023-05-18 13:41:41 +0200

    Realizzata classe RomanPrinter e VERSIONE_1-10 per numeri da 1 a 10 close #8 close #9

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -3,14 +3,80 @@
 // Dmitry Pluzhnikov 1169886
 ////////////////////////////////////////////////////////////////////
 
-////////////////////   V-6   ////////////////////
+////////////////////   V-10   ////////////////////
 
 package it.unipd.mtss;
 
 public class RomanPrinter {
 
-    public static String print(int num) {
-        return printAsciiArt(IntegerToRoman.convert(num));
+    protected static String print_I() {
+        String str_I = " _____\n"+
+                "|_   _|\n"+
+                "  | |\n"+
+                "  | |\n"+
+                " _| |_\n"+
+                "|_____|\n";
+
+        System.out.println(str_I);
+        return str_I;
+    }
+
+    protected static String print_V() {
+        String str_V = "__      __\n"+
+                "\\ \\    / /\n"+
+                " \\ \\  / /\n"+
+                "  \\ \\/ /\n"+
+                "   \\  /\n"+
+                "    \\/\n";
+
+        System.out.println(str_V);
+        return str_V;
+    }
+
+    protected static String print_X() {
+        String str_X = "__    __\n"+
+                "\\ \\  / /\n"+
+                " \\ \\/ /\n"+
+                "  > <\n"+
+                " / . \\\n"+
+                "/_/ \\_\\\n";
+
+        System.out.println(str_X);
+        return str_X;
+    }
+
+    protected static boolean isStrangeInput(String romanNumber) {
+        boolean correctInput = true;
+        if(romanNumber == null) { correctInput = false; }
+
+        else if(romanNumber == " ") { correctInput = false; }
+
+        else if(romanNumber.length() < 1 || romanNumber.length() > 4) {
+            correctInput = false;
+        }
+
+        return correctInput;
+    }
+
+    protected static boolean isInputCorrect(String romanNumber) {
+        boolean correctInput = true;
+
+        if(!isStrangeInput(romanNumber)){correctInput = false;}
+
+        else if(romanNumber.length() > 1 && romanNumber.charAt(0) == 'X') {
+            correctInput = false;
+        }
+
+        else if(romanNumber.length() > 2 && romanNumber.charAt(1) == 'X') {
+            correctInput = false;
+        }
+
+        for (int i = 0; correctInput && i < romanNumber.length(); i++){
+            char aux = romanNumber.charAt(i);
+            if( aux != 'I' && aux != 'V' && aux != 'X') {correctInput = false;}
+        }
+
+        return correctInput;
     }
 
     protected static String printAsciiArt(String romanNumber) {
@@ -22,55 +88,15 @@ public class RomanPrinter {
                 char aux = romanNumber.charAt(i);
                 if(aux == 'I'){ stringaAscii += print_I(); }
                 else if(aux == 'V') { stringaAscii += print_V(); }
+                else if(aux == 'X') { stringaAscii += print_X(); }
             }
         }
 
         return correctInput ? stringaAscii : null;
     }
 
-    protected static boolean isInputCorrect(String romanNumber) {
-        boolean correctInput = true;
-
-        if(romanNumber == null) { correctInput = false; }
-
-        else if(romanNumber.length() < 1 || romanNumber.length() > 3) {
-            correctInput = false;
-        }
-
-        else if(romanNumber.charAt(0) == 'V' && romanNumber.length() > 2) {
-            correctInput = false;
-        }
-
-        for (int i = 0; correctInput && i < romanNumber.length(); i++){
-            char aux = romanNumber.charAt(i);
-            if( aux != 'I' && aux != 'V') { correctInput = false; }
-        }
-
-        return correctInput;
-    }
-
-    protected static String print_I() {
-        String str_I = " _____\n"+
-                       "|_   _|\n"+
-                       "  | |\n"+
-                       "  | |\n"+
-                       " _| |_\n"+
-                       "|_____|\n";
-
-        System.out.println(str_I);
-        return str_I;
-    }
-
-    protected static String print_V() {
-        String str_V = "__      __\n"+
-                       "\\ \\    / /\n"+
-                       " \\ \\  / /\n"+
-                       "  \\ \\/ /\n"+
-                       "   \\  /\n"+
-                       "    \\/\n";
-
-        System.out.println(str_V);
-        return str_V;
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
     }
 
 
