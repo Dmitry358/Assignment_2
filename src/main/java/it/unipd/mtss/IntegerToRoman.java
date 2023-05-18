d6985a05ea1d11e97fba7c3756f8d6e80bb495e1
Dmitry358 <Dmitry358@github.com>
2023-05-18 13:46:11 +0200

    Realizzata classe IntegerToRomain versione da 1 a 20 numeri close #10

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -3,7 +3,7 @@
 // Dmitry Pluzhnikov 1169886
 ////////////////////////////////////////////////////////////////////
 
-////////////////////   V-10   ////////////////////
+////////////////////   V-20   ////////////////////
 
 package it.unipd.mtss;
 
@@ -11,21 +11,55 @@ import java.lang.String;
 
 public class IntegerToRoman{
 
-    public static String convert(int number){
-        String result = null;
-        if(number == 1) { result = "I"; }
-        else if(number == 2) { result = "II"; }
-        else if(number == 3) { result = "III"; }
-        else if(number == 4) { result = "IV"; }
-        else if(number == 5) { result = "V"; }
-        else if(number == 6) { result = "VI"; }
-        else if(number == 7) { result = "VII"; }
-        else if(number == 8) { result = "VIII"; }
-        else if(number == 9) { result = "IX"; }
-        else if(number == 10) { result = "X"; }
-
-        return result;
+    protected static String stampaDaUnoATre(int number){
+        String result = "";
+        for(int i = 1; i <= number; i++){
+            result += "I";
+        }
+        return  result;
     }
 
+    protected static String stampaDaSeiAOtto(int number){
+        String result = "V";
+        for(int i = 1; i <= number-5; i++){
+            result += "I";
+        }
+        return  result;
+    }
+
+    protected static String numUni(int number){
+        String result = "";
+
+        if(number>=1 && number <=3){
+            result = stampaDaUnoATre(number);
+        }
+
+        else if(number == 4) { result = "IV"; }
+        else if(number == 5) { result = "V"; }
+
+        else if(number>=6 && number <=8){
+            result = stampaDaSeiAOtto(number);
+        }
+
+        else if(number == 9) { result = "IX"; }
+
+        return result.isEmpty() ? null : result;
+    }
+
+    public static String convert(int number) {
+        if(number > 20 || number < 1) { return null; }
+        String result = "";
+
+       // 20 >= num_dec >= 1
+       for (int i = 1; i <= number / 10; i++) { result += "X"; }
+
+       String uni = numUni(number % 10);
+
+       if(uni != null) { result+=uni; }
+
+       return result.isEmpty() ? null : result;
+    }
 
 }
+
+
